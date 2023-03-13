f0d6cf5dd0d00ad985703286c26613f33b6a5c69
Kateryna Klimchuk <ketyclima@gmail.com>
2023-03-13 17:06:40 +0100

    Add main icons and toasts styles

--- toast/src/App.tsx
+++ toast/src/App.tsx
@@ -1,6 +1,23 @@
 import { useState } from "react";
 import { ToastCard } from "./components/toast/toastCard";
 
+export const DownArrow = () => (
+  <svg
+    className="text-blue-600 w-5 h-5"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M7 13L12 18L17 13M7 6L12 11L17 6"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
 export const Toast = () => {
   const [showNotification, setShowNotification] = useState(false);
 
@@ -8,17 +25,25 @@ export const Toast = () => {
     setShowNotification(!showNotification);
   };
   return (
-    <div className="text-center">
-      <h1 className="p-2 mb-2">
-        The library, which help you to provide the best notification
-      </h1>
+    <div className="flex flex-col items-center">
+      <div className="p-2 mb-2 flex items-center">
+        <h1 className="mr-2">Please, press the button to see a notification</h1>
+        <DownArrow />
+      </div>
       <button
         onClick={onHandleClick}
         className="border rounded border-spacing-2 border-orange-500 px-2 py-1 mb-2 bg-orange-300"
       >
-        Add notification
+        Add a notification
       </button>
-      {showNotification && <ToastCard />}
+      {showNotification && (
+        <ToastCard
+          type={"success"}
+          title={"Hooray"}
+          fullColor
+          description={"This notification was successfully shown!"}
+        />
+      )}
     </div>
   );
 };
--- toast/src/components/toast/toastCard.tsx
+++ toast/src/components/toast/toastCard.tsx
@@ -1,3 +1,78 @@
-export const ToastCard: React.FunctionComponent = () => {
-  return <h1>Some new notification</h1>;
+import { z } from "zod";
+import clsx from "clsx";
+import { SuccessIcon } from "../icon/SuccessIcon";
+import { ErrorIcon } from "../icon/ErrorIcon";
+import { InfoIcon } from "../icon/InfoIcon";
+import { useState } from "react";
+
+export const ToastSchema = z.object({
+  type: z.enum(["success", "error", "info", "loading"]),
+  title: z.string().optional(),
+  description: z.string(),
+  fullColor: z.boolean().optional(),
+  position: z
+    .enum(["top-right", "top-left", "bottom-right", "bottom-left"])
+    .optional(),
+});
+
+export type ToastType = z.infer<typeof ToastSchema>;
+
+export const ToastCard: React.FunctionComponent<ToastType> = ({
+  type,
+  title,
+  description,
+  fullColor,
+  position = "bottom-right",
+}) => {
+  const [close, setClose] = useState(false);
+
+  const [containerStyle] = {
+    success: ["bg-green-50 hover:bg-green-100"],
+    error: ["bg-red-50 hover:bg-red-100"],
+    info: ["bg-yellow-50 hover:bg-yellow-100"],
+    loading: [],
+  }[type];
+
+  const [toastPosition]: any = {
+    "top-right": ["right-4 top-2"],
+    "top-left": ["left-4 top-2"],
+    "bottom-right": ["right-4 bottom-2"],
+    "bottom-left": ["left-4 bottom-2"],
+  }[position];
+
+  const onHandleCloseNotification = () => {
+    setClose(true);
+    setTimeout(() => {}, 300);
+  };
+
+  return (
+    <div
+      className={clsx(
+        "border rounded w-96 p-4 fixed shadow-md hover:bg-gray-50",
+        toastPosition,
+        close ? "invisible" : fullColor ? containerStyle : ""
+      )}
+    >
+      <button
+        className="absolute top-0 right-2 rotate-45 text-gray-400 hover:text-gray-500"
+        onClick={onHandleCloseNotification}
+      >
+        +
+      </button>
+
+      <div className="flex items-center">
+        {type === "success" ? (
+          <SuccessIcon />
+        ) : type === "error" ? (
+          <ErrorIcon />
+        ) : (
+          <InfoIcon />
+        )}
+        <div>
+          <p className="font-medium">{title}</p>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
